400dbdde2bbefb6646bc840e5bc70d54dfad3434
kapopay <nawfal.yessoufou@epitech.eu>
2024-09-25 01:27:07 +0200

    finish ex04

---
+++ ex_04.js
@@ -0,0 +1,7 @@
+const text1 = 'Welcome to the javascript pool!';
+const text2 = 'Have some fun and dont forget to bring a towel.';
+
+/*Website launched!*/
+
+console.log(text1);
+console.log(text2);
